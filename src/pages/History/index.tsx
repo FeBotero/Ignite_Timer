f9e716475fe700bf342510124f873c0f0f7a392d
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-06-09 18:07:33 -0400

    Pagina:History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,41 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
   return (
-    <>
-      <h1>History</h1>
-    </>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>15 minutos</td>
+              <td>Há cerca de 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>15 minutos</td>
+              <td>Há cerca de 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>15 minutos</td>
+              <td>Há cerca de 1 mês</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
