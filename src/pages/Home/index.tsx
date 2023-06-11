119494f61b8eac4bc0366a1a6fb39145b9bd904b
Felipe Botero <59348629+FeBotero@users.noreply.github.com>
2023-06-11 17:29:45 -0400

    start new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { useState } from 'react'
 
 import {
   CountDownContainer,
@@ -24,7 +25,16 @@ const newCycleFormVaditionSchema = zod.object({
 // Forma de criar tipagem atraves do zod extraindo do schema de validação.
 type NewCycleFormData = zod.infer<typeof newCycleFormVaditionSchema>
 
+interface Cycle {
+  id: string
+  task: String
+  minutesAmount: Number
+}
+
 export function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
   const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormVaditionSchema),
     defaultValues: {
@@ -34,13 +44,26 @@ export function Home() {
   })
 
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data)
+    const id = String(new Date().getTime())
 
+    const newCycle: Cycle = {
+      // pega a data atual em milisegundos
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+    setCycles((state) => [...state, newCycle])
+    // Diz qual ciclo está ativo
+    setActiveCycleId(id)
     reset()
   }
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  console.log(activeCycle)
   const task = watch('task')
   const minutesAmount = watch('minutesAmount')
+
   const isSubmitDisabled = !task || !minutesAmount
 
   return (
